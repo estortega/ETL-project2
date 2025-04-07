d370849813a258cabbcf618e1abed2c83beafe10
estortega <151706907+estortega@users.noreply.github.com>
2025-04-06 23:24:45 -0500

    Add files via upload

---
+++ ERD/crowdfunding_db_schema.sql
@@ -0,0 +1,78 @@
+DROP DATABASE IF EXISTS crowdfunding_db ;
+CREATE DATABASE crowdfunding_db;
+\c crowdfunding_db
+DROP TABLE IF EXISTS campaign;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS subcategory;
+DROP TABLE IF EXISTS contacts;
+
+CREATE TABLE "campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR(255)   NOT NULL,
+    "description" TEXT   NOT NULL,
+    "goal" FLOAT   NOT NULL,
+    "pledged" FLOAT   NOT NULL,
+    "outcome" VARCHAR(50)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR(50)   NOT NULL,
+    "currency" VARCHAR(50)   NOT NULL,
+    "launch_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR   NOT NULL,
+    "subcategory_id" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR(50)   NOT NULL,
+    "category" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(50)   NOT NULL,
+    "subcategory" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" INT   NOT NULL,
+    "first_name" VARCHAR(255)   NOT NULL,
+    "last_name" VARCHAR(255)   NOT NULL,
+    "email" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
+\copy category from '../Resources/category.csv' WITH DELIMITER ',' CSV HEADER;
+
+SELECT * FROM category LIMIT 5;
+
+\copy subcategory from '../Resources/subcategory.csv' WITH DELIMITER ',' CSV HEADER;
+
+SELECT * FROM subcategory LIMIT 5;
+
+\copy contacts from '../Resources/contacts.csv' WITH DELIMITER ',' CSV HEADER;
+
+SELECT * FROM contacts LIMIT 5;
+
+\copy campaign from '../Resources/campaign.csv' WITH DELIMITER ',' CSV HEADER;
+
+SELECT * FROM campaign LIMIT 5;
